6b936769e8e93ec0f3dd3b52c52542cd9ad86f7c
jester <gujjup0003@gmail.com>
2025-01-04 17:48:28 +0530

    imporove the import statements

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { ShoppingCart } from 'lucide-react'
 import Checkout from './Checkout'
 import Navbar from '../components/Navbar'
-import { Footer } from '../components/footer';
+import { Footer } from '../components/Footer';
 
 const products = [
   {
